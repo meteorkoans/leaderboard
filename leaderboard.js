f14334bb667ce7072ef1bc4d7a0561b89306c081
Calvin Cheng <linchuan.cheng@gmail.com>
2015-07-13 15:06:58 +0800

    Booya, another CRUD example

--- leaderboard.js
+++ leaderboard.js
@@ -31,6 +31,22 @@ if (Meteor.isClient) {
     'click .decrement': function() {
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+    },
+    'click .remove': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove(selectedPlayer);
+    }
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      console.log(playerNameVar);
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      });
     }
   });
 
