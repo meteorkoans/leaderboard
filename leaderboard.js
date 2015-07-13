184a4b39f0a3eebc4d0ca6a2ae78e832247ddcf2
Calvin Cheng <linchuan.cheng@gmail.com>
2015-07-13 14:40:57 +0800

    updating collections and reactive changes on the template + sorting the collections to display

--- leaderboard.js
+++ leaderboard.js
@@ -4,7 +4,7 @@ if (Meteor.isClient) {
 
   Template.leaderboard.helpers({
       player: function() {
-        return PlayersList.find();
+        return PlayersList.find({}, {sort: {score: -1, name: 1}});
       },
       selectedClass: function() {
         var playerId = this._id;
@@ -19,6 +19,14 @@ if (Meteor.isClient) {
     'click .player': function() {
       var playerId = this._id
       Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function() {
+       var selectedPlayer = Session.get('selectedPlayer');
+       PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+    },
+    'click .decrement': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: -5}});
     }
   });
 
